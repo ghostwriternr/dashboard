0b941cae3855589967d676bed1d2fc7b93c0b172
Naresh <ghostwriternr@gmail.com>
2018-02-17 03:26:02 +0530

    Add ability to add notice to calendar

--- src/components/Noticecard.jsx
+++ src/components/Noticecard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import linkifyHtml from 'linkifyjs/html';
+import chrono from 'chrono-node';
 import '../styles/noticeboard.css'
 
 class Noticecard extends Component {
@@ -11,8 +12,42 @@ class Noticecard extends Component {
       content: props.content,
       attachment: props.attachment
     }
-    console.log(this.state);
+    this.parseDateFromText = this.parseDateFromText.bind(this);
+    this.parseDateFromText(this.state.content);
   }
+
+  buildCalendarEvent(parsedEvent) {
+    let base_url = 'https://calendar.google.com/calendar/render?action=TEMPLATE&';
+    let params = 'dates=' + parsedEvent.start.moment().format('YYYYMMDD[T]HHmmss[Z]');
+    if (parsedEvent.hasOwnProperty('end')) {
+      params += '/' + parsedEvent.end.moment().format('YYYYMMDD[T]HHmmss[Z]');
+    }
+    params += '&&';
+    params += 'text=' + encodeURIComponent(this.state.title);
+    return base_url + params;
+  }
+
+  parseDateFromText(textToParse) {
+    let offset = 0;
+    const defaultTags = {
+      opening: "<a class='tooltip'>",
+      closing: "<span class='tooltiptext'>Add to calendar</span></a>"
+    }
+    const parseResult = chrono.parse(textToParse);
+    for (var i=0; i<parseResult.length; ++i) {
+      const result = parseResult[i];
+      const eventLink = this.buildCalendarEvent(result);
+      const tagsToAdd = defaultTags;
+      tagsToAdd.opening = tagsToAdd.opening.slice(0, 3) + "href='" + eventLink + "' target='_blank' " + tagsToAdd.opening.slice(3);
+      textToParse = textToParse.slice(0, result.index + offset) + tagsToAdd.opening + textToParse.slice(result.index + offset);
+      offset += tagsToAdd.opening.length;
+      textToParse = textToParse.slice(
+        0, result.index + result.text.length + offset) + tagsToAdd.closing + textToParse.slice(result.index + result.text.length + offset);
+      offset += tagsToAdd.closing.length;
+    }
+    return textToParse;
+  }
+
   render() {
     return (
       <div className="notice-card">
@@ -30,7 +65,7 @@ class Noticecard extends Component {
             }
           })()}
           <br />
-          <div className="card-content" dangerouslySetInnerHTML={{__html: linkifyHtml(this.state.content)}} />
+          <div className="card-content" dangerouslySetInnerHTML={{__html: linkifyHtml(this.parseDateFromText(this.state.content))}} />
         </div>
         {(() => {
           if (this.state.attachment) {
